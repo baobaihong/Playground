09205ffb68a6ff2618f8ff5989ffba8fc5f3ba7b
Jason Bao <baihongbao@outlook.com>
2024-01-24 13:44:17 +0800

    Escaping closure practice

--- Playground/EscapingClosure.swift
+++ Playground/EscapingClosure.swift
@@ -11,16 +11,64 @@ import SwiftUI
 class EscapingViewModel {
     var text: String = "Hello"
     
+    // Iteration1: instant return function
+    func downloadData() -> String {
+        return "new Data!"
+    }
+    
     func getData() {
         let newData = downloadData()
         text = newData
     }
     
-    func downloadData() -> String {
-        return "new Data!"
+    // Iteration2: closure data retrieving
+    // set a closure as parameter, and call the closure inside the function.
+    // completionHandler takes new data as parameter, and change the text attribute.
+    func downloadData2(completionHandler: (_ data: String) -> Void) {
+        completionHandler("New data!")
     }
     
+    func getData2() {
+        downloadData2 { returnedData in
+            text = returnedData
+        }
+    }
+    
+    // Iteration3: simulation of asynchronous data-fetching
+    func downloadData3(completionHandler: @escaping (_ data: String) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: DispatchWorkItem(block: {
+            completionHandler("new data!")
+        }))
+    }
+    
+    func getData3() {
+        // [weak self] ensures that the reference between closure and class instance is weak instead of strong.
+        // so that when waiting for data-fetching, the class instance can be de-initialize.
+        downloadData3 { [weak self] data in
+            self?.text = data
+        }
+    }
+    
+    // Iteration4: define custom closure parameter data type and typealias, more efficient for multiple data type incoming.
+    func downloadData4(completionHandler: @escaping DownloadCompletion) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: DispatchWorkItem(block: {
+            let result = DownloadResult(data: "New Data")
+            completionHandler(result)
+        }))
+    }
+    
+    func getData4() {
+        downloadData4 { [weak self] (returnedData) in
+            self?.text = returnedData.data
+        }
+    }
 }
+
+struct DownloadResult {
+    let data: String
+}
+
+typealias DownloadCompletion = (DownloadResult) -> ()
 
 struct ContentView: View {
     @State var vm = EscapingViewModel()
@@ -34,7 +82,7 @@ struct ContentView: View {
         }
         .padding()
         .onTapGesture {
-            vm.getData()
+            vm.getData4()
         }
     }
 }
