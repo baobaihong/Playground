e698bf65a47e66fbe05a011052dd2e261531de57
Jason Bao <baihongbao@outlook.com>
2024-01-29 18:12:49 +0800

    Publisher/subscribe explanation & initial code

--- Playground/DownloadWithCombine.swift
+++ Playground/DownloadWithCombine.swift
@@ -6,16 +6,80 @@
 //
 
 import SwiftUI
+import Combine
 
-// 1234
-struct DownloadWithCombine: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Observable
+class DownloadWithCombineViewModel {
+    
+    var posts: [PostModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        // Combine discussion:
+        /*
+        //1. sign up for monthly subscription for package to be delivered
+        //2. the company would make the package behind the scene
+        //3. receive the package at your front door
+        //4. make sure the box is intact
+        //5. open and make sure the item is correct
+        //6. use the item
+        //7. cancellable at any time
+        
+        //1. Create the the publisher
+        //2. subscribe the publisher onto the background thread
+        //3.  receive on main thread
+        //4. tryMap (check the data is intact)
+        //5. decode (decode data into PostModels)
+        //6. sink ( put the item into our app)
+        //7. store (cancel subscription if needed)
+         */
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap { data, response -> Data in
+                guard
+                    let response = response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: [PostModel].self, decoder: JSONDecoder())
+            .sink { completion in
+                print("completion: \(completion)")
+            } receiveValue: { [weak self] returnPosts in
+                self?.posts = returnPosts
+            }
+            .store(in: &cancellables)
+       
     }
 }
-//asdfasdfasdfas
-//asdfasdfasdfsadfasdf
-//dummy change, don't use amend...
+
+struct DownloadWithCombine: View {
+    
+    @State var vm = DownloadWithCombineViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(vm.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title).font(.headline)
+                    Text(post.body).foregroundStyle(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
+
 #Preview {
     DownloadWithCombine()
 }
