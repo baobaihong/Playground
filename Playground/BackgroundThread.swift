d2ae163fe1a0d43a6ce8b8015a161dc5e08a12e5
Jason Bao <baihongbao@outlook.com>
2024-01-29 11:53:23 +0800

    Background Thread

---
+++ Playground/BackgroundThread.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundThread.swift
+//  Playground
+//
+//  Created by Jason on 2024/1/29.
+//
+
+import SwiftUI
+
+struct BackgroundThread: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundThread()
+}
