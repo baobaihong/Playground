8b06999592c3992853056ceafa4ead4e9f5fe643
Jason Bao <baihongbao@outlook.com>
2024-01-30 13:38:30 +0800

    Timer practice final version

--- Playground/TimerView.swift
+++ Playground/TimerView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct TimerView: View {
     
-    let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    //publisher that repeatedly emits the current date on the given interval
+    let timer = Timer.publish(every: 3.0, on: .main, in: .common).autoconnect()
     
-    // Current Time
+    // Current Time Showcase code
     /*
     @State var currentDate: Date = Date()
     var dateFormatter: DateFormatter {
@@ -20,11 +21,30 @@ struct TimerView: View {
         formatter.timeStyle = .full
         return formatter
     }
-     */
+*/
     
-    // Countdown
+    // Countdown to second
+    /*
     @State var count: Int = 10
     @State var finishedText: String? = nil
+     */
+    
+    // Countdown to date
+    /*
+    @State var timeRemaining: String = ""
+    let futureDate: Date = Calendar.current.date(byAdding: .hour, value: 1, to: Date()) ?? Date()
+    
+    func updateTimeRemaining() {
+        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
+        let hour = remaining.hour ?? 0
+        let minute = remaining.minute ?? 0
+        let second = remaining.second ?? 0
+        timeRemaining = "\(hour):\(minute):\(second)"
+    }
+     */
+    
+    // Loading Animation counter
+    @State var count: Int = 1
     
     var body: some View {
         ZStack {
@@ -35,17 +55,49 @@ struct TimerView: View {
                 endRadius: 500)
             .ignoresSafeArea()
             
-            Text(finishedText ?? "\(count)")
-                .font(.system(size: 100, weight: .semibold, design: .rounded))
-                .foregroundStyle(.white)
-                .lineLimit(1)
-                .minimumScaleFactor(0.1)
+//            Text(timeRemaining)
+//                .font(.system(size: 100, weight: .semibold, design: .rounded))
+//                .foregroundStyle(.white)
+//                .lineLimit(1)
+//                .minimumScaleFactor(0.1)
+            
+//            HStack(spacing: 15) {
+//                Circle()
+//                    .offset(y: count == 1 ? -20 : 0)
+//                Circle()
+//                    .offset(y: count == 2 ? -20 : 0)
+//                Circle()
+//                    .offset(y: count == 3 ? -20 : 0)
+//            }
+//            .frame(width: 200)
+//            .foregroundStyle(.white)
+            
+            // Auto scroll header, accomplished by Timer
+            TabView(selection: $count,
+                    content:  {
+                Rectangle()
+                    .foregroundStyle(.red)
+                    .tag(1)
+                Rectangle()
+                    .foregroundStyle(.blue)
+                    .tag(2)
+                Rectangle()
+                    .foregroundStyle(.green)
+                    .tag(3)
+                Rectangle()
+                    .foregroundStyle(.orange)
+                    .tag(4)
+                Rectangle()
+                    .foregroundStyle(.pink)
+                    .tag(5)
+            })
+            .frame(height: 200)
+            .tabViewStyle(PageTabViewStyle())
         }
         .onReceive(timer, perform: { _ in
-            if count <= 1 {
-                finishedText = "Boom!!"
-            } else {
-                count -= 1
+            withAnimation(.default) {
+                // a short version of 'if count == 3 { count = 0 } else { count += 1 }'
+                count = count == 5 ? 1 : count + 1
             }
         })
     }
