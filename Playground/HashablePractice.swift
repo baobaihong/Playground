c1c2019bc2f0bb36f30ef1eb86404684c5623890
Jason Bao <baihongbao@outlook.com>
2024-02-21 17:54:06 +0800

    Hashable explaination

--- Playground/HashablePractice.swift
+++ Playground/HashablePractice.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
+struct MyCustomModel: Hashable { //  for some reason you don't want to use identifiable, you can use hashable protocol and create a func to generate hash value
+    // let id = UUID().uuidString
+    let title: String
+    // create hash value
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
 struct HashablePractice: View {
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "ONE"),
+        MyCustomModel(title: "TWO"),
+        MyCustomModel(title: "THREE"),
+        MyCustomModel(title: "FOUR"),
+        MyCustomModel(title: "FIVE"),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 40.0) {
+                ForEach(data, id: \.self) { item in // you can use self as id because string originally conform to hashable
+                    Text(item.hashValue.description)
+                        .font(.headline)
+                }
+            }
+        }
     }
 }
 
